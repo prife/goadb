557d2ad16bde3dead6cef64fc92015dd6f2e62b2
zhongkaizhu <zhongkaizhu@tencent.com>
2023-11-22 13:26:34 +0800

    add discovery services

---
+++ services/discovery.go
@@ -0,0 +1,62 @@
+package services
+
+import (
+	"context"
+
+	"github.com/prife/gomlib"
+	log "github.com/sirupsen/logrus"
+	adb "github.com/zach-klippenstein/goadb"
+)
+
+func InitAdb() (cli *adb.Adb, err error) {
+	serverConfig := adb.ServerConfig{
+		AutoStart: true,
+		Host:      "127.0.0.1",
+		Port:      5037,
+	}
+
+	/*
+		out, err := exec.Command("adb", "start-server").CombinedOutput()
+		if err != nil {
+			panic(out)
+		}
+	*/
+
+	cli, err = adb.NewWithConfig(serverConfig)
+	if err != nil {
+		log.Errorln(err)
+		return
+	}
+
+	err = cli.StartServer()
+	if err != nil {
+		log.Errorln(err)
+		return
+	}
+	return
+}
+
+func Monitor(ctx context.Context, r *gomlib.Registry) (err error) {
+	client, err := InitAdb()
+	if err != nil {
+		return
+	}
+
+	watcher := client.NewDeviceWatcher()
+	for event := range watcher.C() {
+		log.Infof("adb-monitor: %+v", event)
+		switch event.NewState {
+		case adb.StateOnline:
+		case adb.StateInvalid,
+			adb.StateUnauthorized,
+			adb.StateAuthorizing,
+			adb.StateDisconnected,
+			adb.StateOffline,
+			adb.StateHost:
+		default:
+			log.Fatalf("adb-monitor: unknown listen message type: %#v", event)
+		}
+	}
+
+	return watcher.Err()
+}
---
+++ services/discovery_test.go
@@ -0,0 +1,39 @@
+package services_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/prife/gomlib"
+	log "github.com/sirupsen/logrus"
+	"github.com/zach-klippenstein/goadb/services"
+)
+
+func TestMonitor(t *testing.T) {
+	// ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	// defer cancel()
+	ctx := context.Background()
+
+	registry := gomlib.NewRegistry()
+	registry.Listen(gomlib.NewDeviceListener(
+		func(ctx context.Context, d gomlib.DeviceEntry) {
+			log.Infof("--> added device:%#v", d)
+		}, func(ctx context.Context, d gomlib.DeviceEntry) {
+			log.Infof("--> removed device:%#v", d)
+		},
+	))
+
+	ch := make(chan error)
+	go func() {
+		log.Infoln("=== adb monitor begin === ")
+		err := services.Monitor(ctx, registry)
+		log.Errorln("=== adb monitor quit ===", err)
+		ch <- err
+	}()
+
+	select {
+	case <-ctx.Done():
+	case err := <-ch:
+		t.Fatalf("quit:%v", err)
+	}
+}
