9879f2d10f53564e1536f42f60abcd1934970cff
zhongkaizhu <zhongkaizhu@tencent.com>
2024-05-15 20:49:32 +0800

    Refactor wire/sync_conn.go

--- wire/sync_conn.go
+++ wire/sync_conn.go
@@ -1,30 +1,23 @@
 package wire
 
-import "github.com/prife/goadb/internal/errors"
+import (
+	"fmt"
+)
 
 const (
 	// Chunks cannot be longer than 64k.
 	SyncMaxChunkSize = 64 * 1024
 )
 
-/*
-SyncConn is a connection to the adb server in sync mode.
-Assumes the connection has been put into sync mode (by sending "sync" in transport mode).
-
-The adb sync protocol is defined at
-https://android.googlesource.com/platform/system/core/+/master/adb/SYNC.TXT.
-
-Unlike the normal adb protocol (implemented in Conn), the sync protocol is binary.
-Lengths are binary-encoded (little-endian) instead of hex.
-
-# Notes on Encoding
-
-Length headers and other integers are encoded in little-endian, with 32 bits.
-
-File mode seems to be encoded as POSIX file mode.
-
-Modification time seems to be the Unix timestamp format, i.e. seconds since Epoch UTC.
-*/
+// SyncConn is a connection to the adb server in sync mode.
+// Assumes the connection has been put into sync mode (by sending "sync" in transport mode).
+// The adb sync protocol is defined at
+// https://android.googlesource.com/platform/system/core/+/master/adb/SYNC.TXT.
+// Unlike the normal adb protocol (implemented in Conn), the sync protocol is binary.
+// Lengths are binary-encoded (little-endian) instead of hex.
+// Length headers and other integers are encoded in little-endian, with 32 bits.
+// File mode seems to be encoded as POSIX file mode.
+// Modification time seems to be the Unix timestamp format, i.e. seconds since Epoch UTC.
 type SyncConn struct {
 	SyncScanner
 	SyncSender
@@ -32,6 +25,10 @@ type SyncConn struct {
 
 // Close closes both the sender and the scanner, and returns any errors.
 func (c SyncConn) Close() error {
-	return errors.CombineErrs("error closing SyncConn", errors.NetworkError,
-		c.SyncScanner.Close(), c.SyncSender.Close())
+	senderErr := c.SyncSender.Close()
+	scannerErr := c.SyncScanner.Close()
+	if senderErr != nil || scannerErr != nil {
+		return fmt.Errorf("error closing SyncConn: %w, %w", senderErr, scannerErr)
+	}
+	return nil
 }
