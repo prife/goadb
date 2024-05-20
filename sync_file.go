1801f96c2eaece2219e09f9bc68f6e3d3bebb504
zhongkaizhu <zhongkaizhu@tencent.com>
2024-05-21 00:23:00 +0800

    Merge sync_file_writer and sync_file_reader to sync_file

--- sync_file.go
+++ sync_file.go
@@ -8,6 +8,49 @@ import (
 	"github.com/prife/goadb/wire"
 )
 
+// syncFileReader wraps a SyncConn that has requested to receive a file.
+type syncFileReader struct {
+	// Reader used to read data from the adb connection.
+	syncConn *wire.SyncConn
+	toRead   int
+}
+
+var _ io.ReadCloser = &syncFileReader{}
+
+func newSyncFileReader(s *wire.SyncConn) (r io.ReadCloser) {
+	r = &syncFileReader{
+		syncConn: s,
+	}
+	return
+}
+
+func (r *syncFileReader) Read(buf []byte) (n int, err error) {
+	var length int32
+	if r.toRead == 0 {
+		length, err = r.syncConn.ReadNextChunkSize()
+		if err != nil {
+			return 0, err
+		} else {
+			r.toRead = int(length)
+		}
+	}
+
+	// need read `r.toRead` bytes
+	if len(buf) >= int(r.toRead) {
+		n, err = io.ReadFull(r.syncConn, buf[:r.toRead])
+	} else {
+		n, err = io.ReadFull(r.syncConn, buf)
+	}
+
+	r.toRead = r.toRead - n
+	return
+}
+
+// syncFileReader should not close underlying syncConn
+func (r *syncFileReader) Close() error {
+	return nil
+}
+
 // syncFileWriter wraps a SyncConn that has requested to send a file.
 type syncFileWriter struct {
 	// The modification time to write in the footer.
--- sync_file_test.go
+++ sync_file_test.go
@@ -3,6 +3,8 @@ package adb
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
+	"io"
 	"strings"
 	"testing"
 	"time"
@@ -10,6 +12,112 @@ import (
 	"github.com/prife/goadb/wire"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestReadNextChunk(t *testing.T) {
+	s := wire.NewSyncConn(makeMockConnStr(
+		"DATA\006\000\000\000hello DATA\005\000\000\000worldDONE"))
+
+	// Read 1st chunk
+	reader, err := s.ReadNextChunkSize()
+	assert.NoError(t, err)
+	assert.Equal(t, int32(6), reader)
+	buf := make([]byte, 10)
+	n, err := s.Read(buf[:reader])
+	assert.NoError(t, err)
+	assert.Equal(t, 6, n)
+	assert.Equal(t, "hello ", string(buf[:6]))
+
+	// Read 2nd chunk
+	reader, err = s.ReadNextChunkSize()
+	assert.NoError(t, err)
+	assert.Equal(t, int32(5), reader)
+	buf = make([]byte, 10)
+	n, err = s.Read(buf[:reader])
+	assert.NoError(t, err)
+	assert.Equal(t, 5, n)
+	assert.Equal(t, "world", string(buf[:5]))
+
+	// Read DONE
+	_, err = s.ReadNextChunkSize()
+	assert.Equal(t, io.EOF, err)
+}
+func TestReadNextChunkInvalidChunkId(t *testing.T) {
+	s := wire.NewSyncConn(makeMockConnStr(
+		"ATAD\006\000\000\000hello "))
+
+	// Read 1st chunk
+	_, err := s.ReadNextChunkSize()
+	assert.EqualError(t, err, "AssertionError: expected chunk id 'DATA' or 'DONE', but got 'ATAD'")
+}
+
+func TestReadMultipleCalls(t *testing.T) {
+	s := wire.NewSyncConn(makeMockConnStr(
+		"DATA\006\000\000\000hello DATA\005\000\000\000worldDONE"))
+	reader := newSyncFileReader(s)
+
+	firstByte := make([]byte, 1)
+	_, err := io.ReadFull(reader, firstByte)
+	assert.NoError(t, err)
+	assert.Equal(t, "h", string(firstByte))
+
+	restFirstChunkBytes := make([]byte, 5)
+	_, err = io.ReadFull(reader, restFirstChunkBytes)
+	assert.NoError(t, err)
+	assert.Equal(t, "ello ", string(restFirstChunkBytes))
+
+	secondChunkBytes := make([]byte, 5)
+	_, err = io.ReadFull(reader, secondChunkBytes)
+	assert.NoError(t, err)
+	assert.Equal(t, "world", string(secondChunkBytes))
+
+	_, err = io.ReadFull(reader, make([]byte, 5))
+	assert.Equal(t, io.EOF, err)
+}
+
+func TestReadAll(t *testing.T) {
+	s := wire.NewSyncConn(makeMockConnStr(
+		"DATA\006\000\000\000hello DATA\005\000\000\000worldDONE"))
+	reader := newSyncFileReader(s)
+	buf := make([]byte, 20)
+	_, err := io.ReadFull(reader, buf)
+	assert.Equal(t, io.ErrUnexpectedEOF, err)
+	assert.Equal(t, "hello world\000", string(buf[:12]))
+}
+
+func TestReadError(t *testing.T) {
+	s := wire.NewSyncConn(makeMockConnStr(
+		"FAIL\004\000\000\000fail"))
+	r := newSyncFileReader(s)
+	_, err := r.Read(nil)
+	assert.EqualError(t, err, "AdbError: request read-chunk, server error: fail")
+}
+
+func TestReadEmpty(t *testing.T) {
+	s := wire.NewSyncConn(makeMockConnStr(
+		"DONE"))
+	r := newSyncFileReader(s)
+	data, err := io.ReadAll(r)
+	assert.NoError(t, err)
+	assert.Empty(t, data)
+	// Multiple read calls that return EOF is a valid case.
+	for i := 0; i < 5; i++ {
+		data, err := io.ReadAll(r)
+		assert.ErrorIs(t, err, io.EOF)
+		assert.Empty(t, data)
+	}
+}
+
+func TestReadErrorNotFound(t *testing.T) {
+	s := wire.NewSyncConn(makeMockConnStr(
+		"FAIL\031\000\000\000No such file or directory"))
+	r := newSyncFileReader(s)
+	_, err := r.Read(nil)
+	assert.True(t, errors.Is(err, wire.ErrFileNoExist))
+	assert.EqualError(t, err, "FileNoExist: no such file or directory")
+}
+
+////////////////////////////////////////////////////////////////////
+// writer
 
 func TestFileWriterWriteSingleChunk(t *testing.T) {
 	var buf bytes.Buffer
